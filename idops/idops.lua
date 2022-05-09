9d33b6e3c0c7147c46b07d764a48caadf01094fc
RBTS <rbadmin@rabbitboots.com>
2022-05-09 19:19:24 -0300

    remove tabs on empty lines

--- idops/idops.lua
+++ idops/idops.lua
@@ -46,9 +46,9 @@ local function map_imageDataToTemp(x, y, r, g, b, a)
 	temp[temp_i + 2] = g
 	temp[temp_i + 3] = b
 	temp[temp_i + 4] = a
-	
+
 	temp_i = temp_i + 4
-	
+
 	return r, g, b, a
 end
 
@@ -56,7 +56,7 @@ end
 -- scaleIntegral(): Copies temp array values to the scaled destination ImageData.
 local function map_tempToImageData(x, y, r, g, b, a)
 	local i = 1 + (math.floor(y/temp_sh)*temp_w + math.floor(x/temp_sw)) * 4
-	
+
 	return temp[i], temp[i + 1], temp[i + 2], temp[i + 3]
 end
 
@@ -104,28 +104,28 @@ function idops.pasteScaled(src, dst, scale_x, scale_y, src_x, src_y, src_w, src_
 	src_h = src_h or src:getHeight()
 	dst_x = dst_x or 0
 	dst_y = dst_y or 0
-	
+
 	if scale_x <= 0 or scale_y <= 0 then
 		error("scale_x and scale_y must be greater than zero.")
 	end
-	
+
 	local dst_w = math.max(1, math.floor(src_w * scale_x))
 	local dst_h = math.max(1, math.floor(src_h * scale_y))
-	
+
 	local w1, h1 = src:getDimensions()
 	local w2, h2 = dst:getDimensions()
-	
+
 	local dx1 = math.floor(math.max(0, math.min(dst_x, w2 - 1)))
 	local dx2 = math.floor(math.min(dx1 + dst_w, w2 - 1))
-	
+
 	local dy1 = math.floor(math.max(0, math.min(dst_y, h2 - 1)))
 	local dy2 = math.floor(math.min(dy1 + dst_h, h2 - 1))
-	
+
 	for y = dy1, dy2 do
 		for x = dx1, dx2 do
 			local src_x = math.max(0, math.min(math.floor(x / scale_x), w1 - 1))
 			local src_y = math.max(0, math.min(math.floor(y / scale_y), h1 - 1))
-			
+
 			local r, g, b, a = src:getPixel(src_x, src_y)
 			dst:setPixel(x, y, r, g, b, a)
 		end
@@ -135,27 +135,27 @@ end
 
 function idops.scaleIntegral(src, sw, sh)
 	sh = sh or sw
-	
+
 	if sw < 1 or sh < 1 or math.floor(sw) ~= sw or math.floor(sh) ~= sh then
 		error("input scale needs to be integers >= 1.")
 	end
-	
+
 	local w, h = src:getDimensions()
 	local n_w, n_h = w*sw, h*sh
-	
+
 	if n_w < 1 or n_h < 1 then
 		error("the new scaled ImageData dimensions must be at least 1x1.")
 	end
-	
+
 	-- Same dimensions as original: just return a copy
 	if n_w == src:getWidth() and n_h == src:getHeight() then
 		return src:clone()
 	end
-	
+
 	local dst = love.image.newImageData(n_w, n_h)
-	
+
 	idops.pasteScaled(src, dst, sw, sh, 0, 0, src:getWidth(), src:getHeight(), 0, 0)
-	
+
 	return dst
 end
 
@@ -168,27 +168,27 @@ function idops.replaceRGB(src, r1, g1, b1, r2, g2, b2, px, py, pw, ph)
 	assertArgType(5, r2, "number")
 	assertArgType(6, g2, "number")
 	assertArgType(7, b2, "number")
-	
+
 	px = px or 0
 	assertArgType(8, px, "number")
-	
+
 	py = py or 0
 	assertArgType(9, py, "number")
-	
+
 	pw = pw or src:getWidth()
 	assertArgType(10, pw, "number")
-	
+
 	ph = ph or src:getHeight()
 	assertArgType(11, ph, "number")
-	
-	
+
+
 	temp_r1, temp_g1, temp_b1, temp_r2, temp_g2, temp_b2 = r1, g1, b1, r2, g2, b2
 	src:mapPixel(map_replaceRGB, px, py, pw, ph)
 end
 
 
 function idops.replaceRGBA(src, r1, g1, b1, a1, r2, g2, b2, a2, px, py, pw, ph)
-	
+
 	assertArgType(2, r1, "number")
 	assertArgType(3, g1, "number")
 	assertArgType(4, b1, "number")
@@ -197,20 +197,20 @@ function idops.replaceRGBA(src, r1, g1, b1, a1, r2, g2, b2, a2, px, py, pw, ph)
 	assertArgType(7, g2, "number")
 	assertArgType(8, b2, "number")
 	assertArgType(9, a2, "number")
-	
+
 	px = px or 0
 	assertArgType(10, px, "number")
-	
+
 	py = py or 0
 	assertArgType(11, py, "number")
-	
+
 	pw = pw or src:getWidth()
 	assertArgType(12, pw, "number")
-	
+
 	ph = ph or src:getHeight()
 	assertArgType(13, ph, "number")
-	
-	
+
+
 	temp_r1, temp_g1, temp_b1, temp_a1, temp_r2, temp_g2, temp_b2, temp_a2 = r1, g1, b1, a1, r2, g2, b2, a2
 	src:mapPixel(map_replaceRGBA, px, py, pw, ph)
 end
@@ -222,13 +222,13 @@ function idops.addDropShadow(src, ox, oy, tr, tg, tb, ta, sr, sg, sb, sa, ignore
 		for x = 0, w - 1 do
 			local r, g, b, a = src:getPixel(x, y)
 			local is_tr = compColor(r, g, b, a, tr, tg, tb, ta)
-			
+
 			if is_tr then
 				if pixInBounds(x - ox, y - oy, w, h) then
 					local r2, g2, b2, a2 = src:getPixel(x - ox, y - oy)
-					
+
 					local do_set = true
-					
+
 					if ignore_t then
 						for i = 1, #ignore_t do
 							local ig_c = ignore_t[i]
